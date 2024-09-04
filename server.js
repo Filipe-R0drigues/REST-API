25e420fd0cd20bc85eabea81db61c13813889bcd
lipe010 <liperodrigues010@gmail.com>
2024-09-04 17:17:52 -0300

    rotas para cadastro e listagem de usuarios

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-import express from 'express';
+import express, { json } from 'express';
 
 const app = express()
+app.use(express.json())
+
+const usuarios = []
+
+app.post('/usuarios',(req,res)=>{
+    usuarios.push(req.body)
+    res.status(201).json(req.body)
+})
 
 app.get('/usuarios',(req, res)=>{
-    res.send('ok, deu bom.')
+    res.status(200).json(usuarios)
 })
 
 app.listen(3000)
